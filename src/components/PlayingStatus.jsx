5a7809b2f948f55c5c7379b38d5b8722640e9ca2
MustafaAlsadah <sadah9999@gmail.com>
2022-12-02 13:54:01 +0300

    In play badge flashing + deployment setup

--- src/components/PlayingStatus.jsx
+++ src/components/PlayingStatus.jsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import greenCircle from "/src/assets/green_circle.svg";
 import grayCircle from "/src/assets/gray_circle.svg";
+import $ from 'jquery'; 
 
 export default function PlayingStatus(props){
     let gameStatus = props.status;
     let matchIsInPlay = gameStatus!="FINISHED" && gameStatus!="TIMED";
     let badgeText = (matchIsInPlay) ? "In Play" : "Finished";
+
+    $("document").ready(() => {
+        setInterval(() => {
+            $('.green-circle-img').fadeIn(1000);
+            $('.green-circle-img').fadeOut(1000);
+        }, 1);
+    });
+
+
+    
+
     return(
         <div className="playing-status-container">
             <div className="playing-status--top">
-                 {gameStatus=="FINISHED" && <img id="circle-img" src={grayCircle} alt="" />}
-                 <span>{matchIsInPlay && <img id="circle-img" src={greenCircle} alt="" />}</span>
+                 {gameStatus=="FINISHED" && <img id="circle-img" src={grayCircle } alt="" />}
+                 <span className="green-circle-img">{matchIsInPlay && <img id="circle-img" src={greenCircle} alt="" />}</span>
                  <span id="status-badge">{badgeText}</span>
                  
             </div>
